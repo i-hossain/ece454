ca744078f84d693c0bf5aa5a48519b1f0c9dea21
Hossain  Ismail <hossai62@ug167.eecg>
2015-11-07 21:09:09 -0500

    copied contents from list.c to mm.c

--- HW3/assn3-malloc/assn/mm.c
+++ HW3/assn3-malloc/assn/mm.c
@@ -66,40 +66,215 @@ team_t team = {
 
 void* heap_listp = NULL;
 
-int SEG_SIZES[] = {2, 3, 4, 5, 8, 16, 32};
+#define NUM_SEG_LIST 1
 
-#define GET_NEXTP(bp) (*((char *)(bp) + WSIZE))
-#define GET_PREVP(bp) (*(char *)(bp))
+size_t SEG_SIZES[NUM_SEG_LIST] = {1};
+void* sep_list_head[NUM_SEG_LIST];
 
-#define SET_NEXTP(bp, p) (GET_NEXTP(bp) = p) 
-#define SET_PREVP(bp, p) (GET_PREVP(bp) = p)
+typedef struct double_list
+{
+    struct double_list* prev;
+    struct double_list* next;
+}dlist;
 
-void insert (void *bp) {
-    char* head = GET_HEAD();
-    if (bp < head) {
-        //insert before head
-        SET_PREVP(head, bp);
-        SET_NEXTP(bp, head);
-        SET_PREVP(bp, NULL);
-        head = bp;
+void insert_node (void *new_bp)
+{
+    size_t size = GET_SIZE(HDRP(new_bp)) - DSIZE;
+
+    int index, i;
+    for(i = 0; i < NUM_SEG_LIST; i++) {
+        if (size >= SEG_SIZES[i])
+            index = i;
+        else
+            break;
     }
-    else {
-        // insert at sorted location in list
-        char* current = GET_NEXTP(head);
 
-        if (current == NULL) {
-            // one node
-            SET_NEXTP(head, bp);
-            SET_PREVP(bp, head);
-            SET_NEXTP(bp, NULL);
+    printf("Inserting node\n");
+
+    dlist *new_node = (dlist*)new_bp;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+
+    if (sep_list_head[index] == NULL) {
+        sep_list_head[index] = new_bp;
+        return;
+    }
+
+    dlist *head_node = (dlist*)sep_list_head[index];
+      
+    if (new_bp < sep_list_head[index]) {
+        //insert before head
+        printf("insert first\n");
+
+        head_node->prev = new_node;
+        new_node->next = head_node;
+        sep_list_head[index] = new_bp;
+        return;
+    }
+
+    dlist* current = head_node;
+
+    if (current->next == NULL) {
+        // one node
+        printf("insert second\n");
+
+        head_node->next = new_node;
+        new_node->prev = head_node;
+        return;
+    }
+
+    // dlist* current2 = head_node->next;
+
+    // if (current2->next == NULL) {
+    //     // one node
+    //     printf("insert third\n");
+
+    //     head_node->next->next = new_node;
+    //     new_node->prev = head_node->next;
+    //     return;
+    // }
+
+    while (current->next != NULL && new_node > current->next)
+        current = current->next;
+
+    printf("insert mid or last\n");
+
+    new_node->next = current->next;
+    new_node->prev = current;
+
+    if (current->next != NULL)
+        current->next->prev = new_node;
+
+    current->next = new_node;
+}
+
+void remove_node (int index, void *del_bp)
+{
+    dlist *current = (dlist*) del_bp;
+
+    if (current->prev != NULL)
+        current->prev->next = current->next;
+    else
+        sep_list_head[index] = current->next;
+
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+
+    current->prev = NULL;
+    current->next = NULL;
+}
+
+void* search_node (size_t req_size)
+{
+    int sl_index, i;
+    for(i = 0; i < NUM_SEG_LIST; i++) {
+        if (req_size >= SEG_SIZES[i])
+            sl_index = i;
+        else
+            break;
+    }
+
+    while (sl_index < NUM_SEG_LIST) {
+        dlist *current = (dlist*)sep_list_head[sl_index];
+        while (current != NULL) {
+            if (req_size <= GET_SIZE(HDRP((void*)current))) {
+                remove_node(sl_index, (void*)current);
+                return (void*)current;
+            }
+            current = current->next;
         }
+        sl_index++;
+    }
+    return NULL;
+}
 
-        while (GET_NEXT(current != NULL) && bp > GET_NEXT(current))
-            current = GET_NEXTP(current);
+void* seek_and_destroy (size_t req_size)
+{
+    void *bp = search_node(req_size);
 
-        SET_NEXTP(bp, GET_NEXTP(current));
-        SET_PREVP(bp, current);
-        SET_NEXTP(current, bp);
+    if (bp != NULL) {
+        size_t size_bp = GET_SIZE(HDRP(bp));
+    
+        PUT(HDRP(bp), PACK(req_size, 1));
+        PUT(FTRP(bp), PACK(req_size, 1));
+
+        if (req_size < size_bp) {
+            PUT(HDRP(NEXT_BLKP(bp)), PACK(size_bp - req_size, 1));
+            PUT(FTRP(NEXT_BLKP(bp)), PACK(size_bp - req_size, 1));
+            insert_node(NEXT_BLKP(bp));
+        }
+    }
+    // else {
+    //     // extend heap
+    //     if ((bp = extend_heap(req_size/WSIZE)) == NULL)
+    //         return NULL;
+
+    //     PUT(HDRP(bp), PACK(req_size, 1));
+    //     PUT(FTRP(bp), PACK(req_size, 1));
+    // }
+
+    return bp;
+}
+
+void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc) {       /* Case 1 */
+        return bp;
+    }
+
+    size_t curr_size = size - DSIZE;
+    size_t prev_size = GET_SIZE(PREV_BLKP(bp)) - DSIZE;
+    size_t next_size = GET_SIZE(NEXT_BLKP(bp)) - DSIZE;
+
+    int current_index, next_index, prev_index, i;
+    for(i = 0; i < NUM_SEG_LIST; i++) {
+        if (curr_size >= SEG_SIZES[i])
+            current_index = i;
+        if (prev_size >= SEG_SIZES[i])
+            prev_index = i;
+        if (next_size >= SEG_SIZES[i])
+            next_index = i;
+    }
+
+    remove_node(current_index, bp);
+
+    if (prev_alloc && !next_alloc) { /* Case 2 */
+        remove_node(next_index, NEXT_BLKP(bp));
+
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+
+        insert_node(bp);
+        return (bp);
+    }
+
+    else if (!prev_alloc && next_alloc) { /* Case 3 */
+        remove_node(prev_index, PREV_BLKP(bp));
+
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+
+        insert_node(PREV_BLKP(bp));
+        return (PREV_BLKP(bp));
+    }
+
+    else {            /* Case 4 */
+        remove_node(prev_index, PREV_BLKP(bp));
+        remove_node(next_index, NEXT_BLKP(bp));
+
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)))  +
+            GET_SIZE(FTRP(NEXT_BLKP(bp)))  ;
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size,0));
+
+        insert_node(PREV_BLKP(bp));
+        return (PREV_BLKP(bp));
     }
 }
 
